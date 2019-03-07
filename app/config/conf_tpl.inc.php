8b40d22362736bf40da5f8e7779e3e6210eadd97
skphp-ls <48232906+skphp-ls@users.noreply.github.com>
2019-03-07 10:47:59 +0800

    conf_tpl.inc.php
    conf_tpl.inc.php

---
+++ app/config/conf_tpl.inc.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * 模板配置 v2.0.2
+ * 
+ * @auth ls
+ * @mail:56160681@qq.com 
+ * @date:2014-11-25
+ */
+return array(
+
+	// CDN站点	.boyiweichen.com
+	'server_url'					=>	 'http://static.boyiweichen.cn',
+
+	//'file_site_dir'
+	'server_name'					=>	 'qchy', //basename($_SERVER['DOCUMENT_ROOT']),
+
+	// 模板文件夹
+	'tpl_foler'						=>	 '/default',
+
+	// 模板扩展名
+	'tpl_extension'					=>  '.php'
+);
